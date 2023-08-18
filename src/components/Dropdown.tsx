ffb0ea244e2a89c0ae3844164a00b9ccc7f45b1b
Mr Bushido <114846273+fitzies@users.noreply.github.com>
2023-08-18 16:05:03 +0800

    fixed 1

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -5,12 +5,13 @@ const Dropdown = () => {
 
   return (
     <div className="w-full absolute z-50 bg-black -left-2 top-full flex flex-col rounded-lg">
-      {items.map((item) => {
+      {items.map((item, i) => {
         const link = "/browse/" + item.toLowerCase();
         return (
           <Link
             href={link}
             className="pl-1 my-1 mx-2 text-sm cursor-pointer text-gray-300 hover:bg-light py-2 rounded-xl"
+            key={i}
           >
             {item}
           </Link>
